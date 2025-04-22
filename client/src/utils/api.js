3c8f4fe7b808c37734cff7cbdebb055ac76b914b
Devadi Yekaditya <yekaditya.d@gytworkz.com>
2025-04-22 16:59:47 +0530

    added server url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://blog-applicattionserver.vercel.app";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://blog-applicattionserver.vercel.app";
+// export const BASE_URL = "http://localhost:5000";
