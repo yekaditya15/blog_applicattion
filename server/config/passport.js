b8c54f21a9ef9b5d89c4f490a5f2db51a82be054
Devadi Yekaditya <yekaditya.d@gytworkz.com>
2025-04-22 17:07:06 +0530

    google auth server

--- server/config/passport.js
+++ server/config/passport.js
@@ -26,7 +26,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback",
+      callbackURL: `${process.env.SERVER_URL}/api/auth/google/callback`,
       scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
